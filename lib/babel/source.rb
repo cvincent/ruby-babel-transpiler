e84c7d9daace4f443c89e0ecc6b42f3253d0277e
Chris Vincent <chris.vincent@hey.com>
2025-11-05 10:57:02 -0600

    Update and rename source.rb.erb to source.rb

--- lib/babel/source.rb
+++ lib/babel/source.rb
@@ -1,7 +1,7 @@
 module Babel
   module Source
-    VERSION = "<%= version %>"
-    DATE = Time.at(<%= date %>)
+    VERSION = "7.1.5"
+    DATE = Date.new(2025, 11, 5).to_time
     PATH = File.expand_path("../..", __FILE__)
   end
 end
